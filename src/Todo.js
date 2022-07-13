34f21de2ee182ce15d99cee6188e5d2b2e806f75
andredoc <andre.documet@gmail.com>
2022-07-14 01:08:10 +0200

    example useReducer

---
+++ src/Todo.js
@@ -0,0 +1,20 @@
+import {ACTIONS} from './App'
+
+const Todo = ( {todo, dispatch} )=>{
+  return (
+    <>
+      <span style={{ color: todo.complete ? '#AAA' : '#000' }}>
+        {todo.name}
+      </span>
+      <button onClick={()=> dispatch({ type: ACTIONS.TOGGLE_TODO, payload: {id: todo.id}})}>
+        Toggle
+      </button>
+      <button onClick={()=> dispatch({ type: ACTIONS.DELETE_TODO, payload: {id: todo.id}})}>
+        Delete
+      </button>
+    </>
+
+  )
+}
+
+export default Todo; 
